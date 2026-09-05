384261c07addfdf3f417a9fe9dfd4cbe4ed9e95c
Gabriel Funes <gfunes@gmail.com>
2026-09-05 20:55:30 -0300

    modifique el orden de los botones en itemTablaREservas y les cambie el color

--- src/components/services/itemTablaReservas.tsx
+++ src/components/services/itemTablaReservas.tsx
@@ -1,6 +1,6 @@
 const ItemTablaReservas = () => {
   return (
-    <tr className="border-b border-zinc-800 hover:bg-zinc-900/50 transition-colors">
+    <tr className="border-b border-slate-400 hover:bg-slate-400/50 transition-colors">
       <td className="px-6 py-4 whitespace-nowrap text-sm text-zinc-500 font-mono">
         1
       </td>
@@ -15,15 +15,16 @@ const ItemTablaReservas = () => {
       </td>
       <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
         <div className="flex gap-3">
+          <a className="text-blue-500 hover:text-blue-600 transition-colors flex items-center gap-1 cursor-pointer">
+            <i className="bi bi-pencil-square"></i> Confirmar
+          </a>
           <a className="text-green-500 hover:text-green-600 transition-colors flex items-center gap-1 cursor-pointer">
             <i className="bi bi-pencil-square"></i> Editar
           </a>
           <a className="text-red-500 hover:text-red-600 transition-colors flex items-center gap-1 cursor-pointer">
             <i className="bi bi-trash"></i> Borrar
           </a>
-          <a className="text-red-500 hover:text-red-600 transition-colors flex items-center gap-1 cursor-pointer">
-            <i className="bi bi-trash"></i> Confirmar
-          </a>
+          
         </div>
       </td>
     </tr>
