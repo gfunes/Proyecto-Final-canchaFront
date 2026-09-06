535c735e94033639a473dfbc1ec93591c8d8fc0a
Gabriel Funes <gfunes@gmail.com>
2026-09-06 20:45:12 -0300

    se agregaron las rutas protegidas en App

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Footer from "./components/shared/Footer"
 import Menu from "./components/shared/Menu"
 import Error404 from "./components/pages/Error404"
 import { BrowserRouter, Routes, Route } from "react-router";
+import ProtectorRutas from "./components/routes/ProtectorRutas";
 import RegistroUsuario from "./components/pages/RegistroUsuario"
 import AdmReservas from "./components/pages/AdmReservas"
 import AdmCanchas from "./components/pages/AdmCanchas"
@@ -38,15 +39,16 @@ const usuarioSessionStorage = JSON.parse(
           <Routes>
             <Route path="/" element={<Inicio></Inicio>}/>
             <Route path="/login" element={<Login></Login>}/>
-            <Route path="/administrador" element={<Administrador></Administrador>}/>
-            <Route path="/reservas" element={<AdmReservas></AdmReservas>}/>
-            <Route path="/canchas" element={<AdmCanchas></AdmCanchas>}/>
-            <Route path="/administrador/crear" element={<Formulario titulo={'Crear Producto'}></Formulario>}/>
-            <Route path="/administrador/editar/:id" element={<Formulario titulo={'Editar Producto'}></Formulario>}/> */}
-            <Route path="/canchas/crear" element={<FormCancha titulo={'Crear cancha'}></FormCancha>}/>
-            <Route path="/canchas/editar/:id" element={<FormCancha titulo={'Editar cancha'}></FormCancha>}/> */}
-            
             <Route path="/registrate" element={<RegistroUsuario/>} />
+            <Route path="/administrador" element={<ProtectorRutas />}>
+              <Route index element={<Administrador />} />
+             <Route path="/administrador/crear" element={<Formulario titulo={'Crear Producto'}></Formulario>}/>
+              <Route path="/administrador/editar/:id" element={<Formulario titulo={'Editar Producto'}></Formulario>}/> */}
+              <Route path="/administrador/reservas" element={<AdmReservas></AdmReservas>}/>
+            <Route path="/administrador/canchas" element={<AdmCanchas></AdmCanchas>}/>
+            <Route path="/administrador/canchas/crear" element={<FormCancha titulo={'Crear cancha'}></FormCancha>}/>
+            <Route path="/administrador/canchas/editar/:id" element={<FormCancha titulo={'Editar cancha'}></FormCancha>}/> */}
+                   </Route>    
             <Route path="*" element={<Error404></Error404>}/>
           </Routes>
         </main>
