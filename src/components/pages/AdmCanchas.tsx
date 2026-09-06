4ed0e8f97079684502836e55a243cb03968a8424
Gabriel Funes <gfunes@gmail.com>
2026-09-05 22:26:58 -0300

    modifique los titulos del formulario AdmCanchas

--- src/components/pages/AdmCanchas.tsx
+++ src/components/pages/AdmCanchas.tsx
@@ -7,9 +7,9 @@ const AdmCanchas = () => {
       <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 bg-zinc-900/40 p-6 rounded-2xl border border-zinc-800">
         <div>
           <h1 className="text-2xl font-bold text-white">
-            Panel de Administración
+            Panel de Administración Canchas
           </h1>
-          <p className="text-zinc-500 text-sm">Gestion Canchas Alquiler</p>
+          <p className="text-zinc-500 text-sm">Gestion RollingClub</p>
         </div>
         <a className="bg-green-500 hover:bg-green-600 text-white px-3 py-2.5 rounded-xl font-bold transition-all shadow-lg shadow-blue-900/20 active:scale-95 gap-2 text-center">
           <i className="bi bi-plus-lg"></i>
@@ -33,7 +33,10 @@ const AdmCanchas = () => {
                 Categoria
               </th>
               <th className="px-6 py-4 text-xs uppercase tracking-wider text-zinc-500 font-bold text-center">
-                
+                descripcion
+              </th>
+              <th className="px-6 py-4 text-xs uppercase tracking-wider text-zinc-500 font-bold text-center">
+                precio
               </th>
             </tr>
           </thead>
