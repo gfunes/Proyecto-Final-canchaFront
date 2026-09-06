8fcd2d912b467e4fa786f5a21e1f5ba171254f67
Gabriel Funes <gfunes@gmail.com>
2026-09-06 12:18:41 -0300

    modifique el panel administacion para mostrar canchas y productos

--- src/components/pages/Administrador.tsx
+++ src/components/pages/Administrador.tsx
@@ -1,3 +1,4 @@
+import ItemTablaCanchas from "../services/ItemTablaCanchas";
 import ItemTabla from "../services/ItemTablaProducto";
 
 const Administrador = () => {
@@ -7,18 +8,45 @@ const Administrador = () => {
       <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 bg-zinc-900/40 p-6 rounded-2xl border border-zinc-800">
         <div>
           <h1 className="text-2xl font-bold text-white">
-            Panel de Administración
+            Panel de Administración General
           </h1>
-          <p className="text-zinc-500 text-sm">Gestion Catálogo Productos</p>
+          <p className="text-zinc-500 text-sm">Gestion Catálogo RollingClub</p>
         </div>
-        <a className="bg-green-500 hover:bg-green-600 text-white px-3 py-2.5 rounded-xl font-bold transition-all shadow-lg shadow-blue-900/20 active:scale-95 gap-2 text-center">
-          <i className="bi bi-plus-lg"></i>
-          Alta Producto
-        </a>
+        
         
       </div>
 
       {/* Contenedor de la Tabla con Scroll Horizontal para móviles */}
+      <div className="overflow-x-auto rounded-2xl border border-zinc-800 bg-zinc-900/20">
+        <table className="w-full text-left border-collapse">
+          <thead>
+            <tr className="bg-zinc-900/60 border-b border-zinc-800">
+              <th className="px-6 py-4 text-xs uppercase tracking-wider text-zinc-500 font-bold">
+                #
+              </th>
+              <th className="px-6 py-4 text-xs uppercase tracking-wider text-zinc-500 font-bold">
+                Nombre Cancha
+              </th>
+              <th className="px-6 py-4 text-xs uppercase tracking-wider text-zinc-500 font-bold">
+                Tipo Cancha
+              </th>
+              <th className="px-6 py-4 text-xs uppercase tracking-wider text-zinc-500 font-bold text-center">
+                Descripcion
+              </th>
+<th className="px-6 py-4 text-xs uppercase tracking-wider text-zinc-500 font-bold text-center">
+                Acciones
+              </th>
+              <a className="bg-green-500 hover:bg-green-600 text-white px-3 py-2.5 rounded-xl font-bold transition-all shadow-lg shadow-blue-900/20 active:scale-95 gap-2 text-center">
+          <i className="bi bi-plus-lg"></i>
+          Alta Cancha
+        </a>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-zinc-800/50">
+            <ItemTablaCanchas />
+          </tbody>
+        </table>
+      </div>
       <div className="overflow-x-auto rounded-2xl border border-zinc-800 bg-zinc-900/20">
         <table className="w-full text-left border-collapse">
           <thead>
@@ -35,6 +63,10 @@ const Administrador = () => {
               <th className="px-6 py-4 text-xs uppercase tracking-wider text-zinc-500 font-bold text-center">
                 Acciones
               </th>
+              <a className="bg-green-500 hover:bg-green-600 text-white px-3 py-2.5 rounded-xl font-bold transition-all shadow-lg shadow-blue-900/20 active:scale-95 gap-2 text-center">
+          <i className="bi bi-plus-lg"></i>
+          Alta Producto
+        </a>
             </tr>
           </thead>
           <tbody className="divide-y divide-zinc-800/50">
@@ -42,7 +74,6 @@ const Administrador = () => {
           </tbody>
         </table>
       </div>
-      
     </section>
   );
 };
