1e87a2e2ec3ab6f18ed69349fd48834197d4e7bb
Gabriel Funes <gfunes@gmail.com>
2026-09-05 22:28:23 -0300

    agregue color a los botones de ItemTablaCanchas

--- src/components/services/ItemTablaCanchas.tsx
+++ src/components/services/ItemTablaCanchas.tsx
@@ -18,10 +18,8 @@ const ItemTablaCanchas = () => {
       </td>
       <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
         <div className="flex gap-3">
+          
           <a className="text-green-500 hover:text-green-600 transition-colors flex items-center gap-1 cursor-pointer">
-            <i className="bi bi-pencil-square"></i> Confirmar
-          </a>
-          <a className="text-blue-500 hover:text-blue-600 transition-colors flex items-center gap-1 cursor-pointer">
             <i className="bi bi-pencil-square"></i> Editar
           </a>
           <a className="text-red-500 hover:text-red-600 transition-colors flex items-center gap-1 cursor-pointer">
