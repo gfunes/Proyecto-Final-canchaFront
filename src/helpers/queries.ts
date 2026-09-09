59a8d76a03e4a060fbf5ec725e96752f7a102a0c
Gabriel Funes <gfunes@gmail.com>
2026-09-09 11:23:06 -0300

    agregamos querie de cancha

---
+++ src/helpers/queries.ts
@@ -0,0 +1,68 @@
+import type { Cancha } from "../interfaces/canchas";
+
+const urlCanchas = import.meta.env.VITE_ALQUILER_CANCHAS + "/canchas"
+
+export const listarCanchasApi = async ():Promise<Response> =>{
+    try{
+        const respuesta = await fetch(urlCanchas)
+        return respuesta
+    }catch(error){
+        console.error(error)
+        throw error
+    }
+};
+
+export const buscarCanchaApi = async (id:string):Promise<Response> =>{
+    try{
+        const respuesta = await fetch(`${urlCanchas}/${id}`)
+        return respuesta
+    }catch(error){
+        console.error(error)
+        throw error
+    }
+};
+
+export const crearCanchaApi = async (cancha: Cancha):Promise<Response> =>{
+    try{
+        const respuesta = await fetch(urlCanchas, {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(cancha)
+        })
+        return respuesta
+    }catch(error){
+        console.error(error)
+        throw error
+    }
+};
+
+export const editarCanchaApi = async (id:string, cancha: Cancha):Promise<Response> =>{
+    try{
+        const respuesta = await fetch(`${urlCanchas}/${id}`, {
+            method: 'PUT',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(cancha)
+        })
+        return respuesta
+    }catch(error){
+        console.error(error)
+        throw error
+    }
+};
+
+
+export const borrarCanchaApi = async (id:string):Promise<Response> =>{
+    try{
+        const respuesta = await fetch(`${urlCanchas}/${id}`, {
+            method: 'DELETE'
+        })
+        return respuesta
+    }catch(error){
+        console.error(error)
+        throw error
+    }
+};
