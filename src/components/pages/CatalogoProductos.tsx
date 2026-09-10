44010f1d8bde80b841174cde20424c27f6766c07
Ignacio <ignacioholmquist12@gmail.com>
2026-09-10 16:54:59 -0300

    Acomodamos el componente CatalogoProductos y modificamos estilos

--- src/components/pages/CatalogoProductos.tsx
+++ src/components/pages/CatalogoProductos.tsx
@@ -1,6 +1,4 @@
-import SistemaReservas from "./SistemaReservas";
 import CardProducto from "../services/CardProducto";
-
 import {NavLink} from "react-router";
 
 const Inicio = () => {
@@ -9,21 +7,19 @@ const Inicio = () => {
       <section className="space-y-8 animate-fadeIn px-10 my-5">
         <div className="flex flex-col md:flex-row md:items-center justify-between border-b border-zinc-800 pb-5 gap-4">
           <div>
-            <h1 className="text-3xl font-bold text-white tracking-tight">
+            <h1 className="text-3xl font-bold text-white tracking-tight text-center md:text-start">
               Catálogo de <span className="text-green-500">Productos</span>
             </h1>
-            <p className="text-zinc-400 mt-1 text-sm">
+            <p className="text-zinc-400 mt-1 text-sm text-center md:text-start">
               Agrega los productos que quieras al carrito y luego termina tu
               compra
             </p>
           </div>
-          <NavLink to="/" className={"bg-green-500 hover:bg-green-600 transition text-l py-2 px-3 rounded-2xl font-bold cursor-pointer"}>
+          <NavLink to="/" className={"bg-green-500 hover:bg-green-600 transition text-l py-2 px-3 rounded-2xl font-bold cursor-pointer text-center md:text-start"}>
             Volver al juego ⚽
           </NavLink>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <CardProducto/>
-        <CardProducto/>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-around">
         <CardProducto/>
         </div>
       </section>
