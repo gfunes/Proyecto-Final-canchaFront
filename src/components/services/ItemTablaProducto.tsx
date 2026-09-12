422f61345b89f6db1382df9dc442ca3bb4197178
Gabriel Funes <gfunes@gmail.com>
2026-09-12 01:51:25 -0300

    se agrego link de boton editar y borrar al componente itenTablaProducto

--- src/components/services/ItemTablaProducto.tsx
+++ src/components/services/ItemTablaProducto.tsx
@@ -1,3 +1,7 @@
+import { Link } from "react-router";
+import Swal from "sweetalert2";
+import { LuTrash2, LuPencil } from "react-icons/lu";
+
 const ItemTablaProducto = () => {
   return (
     <tr className="border-b border-zinc-800 hover:bg-zinc-900/50 transition-colors">
@@ -16,12 +20,18 @@ const ItemTablaProducto = () => {
       
       <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
         <div className="flex gap-3">
-          <a className="text-green-500 hover:text-green-600 transition-colors flex items-center gap-1 cursor-pointer">
-            <i className="bi bi-pencil-square"></i> Editar
-          </a>
-          <a className="text-red-500 hover:text-red-600 transition-colors flex items-center gap-1 cursor-pointer">
-            <i className="bi bi-trash"></i> Borrar
-          </a>
+          <Link
+           // to={`/administrador/productos/editar/${producto._id}`}
+            className="text-amber-500 hover:text-amber-400 transition-colors flex items-center gap-1"
+          >
+            Editar <LuPencil /> 
+          </Link>
+          <button
+            className="text-red-500 hover:text-red-400 transition-colors flex items-center gap-1"
+          //  onClick={eliminarProducto}
+          >
+            Borrar <LuTrash2 /> 
+          </button>
         </div>
       </td>
     </tr>
