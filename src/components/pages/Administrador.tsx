de7fcc764b792d7810c6a4ab6797ad11c0725fb1
Gabriel Funes <gfunes@gmail.com>
2026-09-12 01:54:15 -0300

    se quito componente productos del panel de administracion

--- src/components/pages/Administrador.tsx
+++ src/components/pages/Administrador.tsx
@@ -96,39 +96,7 @@ const cargarCanchas=async()=>{
           </tbody>
         </table>
       </div>
-      <div className="overflow-x-auto rounded-2xl border border-zinc-800 bg-zinc-900/20">
-        <table className="w-full text-left border-collapse">
-          <thead>
-            <tr className="bg-zinc-900/60 border-b border-zinc-800">
-              <th className="px-6 py-4 text-xs uppercase tracking-wider text-zinc-500 font-bold">
-                #
-              </th>
-              <th className="px-6 py-4 text-xs uppercase tracking-wider text-zinc-500 font-bold">
-               Nombre Producto
-              </th>
-              <th className="px-6 py-4 text-xs uppercase tracking-wider text-zinc-500 font-bold">
-                Tipo Producto
-              </th>
-              <th className="px-6 py-4 text-xs uppercase tracking-wider text-zinc-500 font-bold">
-                Precio
-              </th>
-              <th className="px-6 py-4 text-xs uppercase tracking-wider text-zinc-500 font-bold text-center">
-                Acciones
-              </th>
-              <Link
-          to={"/administrador/crear"}
-          className="bg-slate-400 hover:bg-blue-500 text-white px-5 py-2.5 rounded-xl font-bold transition-all shadow-lg shadow-blue-900/20 active:scale-95 flex items-center gap-2"
-        >
-          <LuCirclePlus />
-          Agregar Producto
-        </Link>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-zinc-800/50">
-            <ItemTabla />
-          </tbody>
-        </table>
-      </div>
+     
     </section>
   );
 };
