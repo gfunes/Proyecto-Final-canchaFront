b4b38496aede457428e2c32c3841b31971527a74
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-09-14 15:06:57 -0300

    agrega Itemtabla querie ListarReservaApi

--- src/components/pages/AdmReservascliente.tsx
+++ src/components/pages/AdmReservascliente.tsx
@@ -1,48 +1,19 @@
-import ItemTablaReserva from "../services/itemTablaReservas";
-import { LuCirclePlus } from "react-icons/lu";
-import { Link } from "react-router";
-import { listarReservasApi } from "../../helpers/queries";
-import type { Reserva } from "../../interfaces/reserva";
+import ItemTabla from "../services/itemTablaReservas";
 
-import { useEffect, useState } from "react";
-import Swal from "sweetalert2";
-
-const AdmReservas = () => {
-  const [reservas, setReservas] = useState<Reserva[]>([]);
-
-  useEffect(() => {
-    cargarReservas();
-  }, []);
-  const cargarReservas = async () => {
-    const respuestaReserva = await listarReservasApi();
-
-    if (respuestaReserva && respuestaReserva.status === 200) {
-      const data = await respuestaReserva.json();
-
-      setReservas(data.reservas);
-      console.log("consulta datos :", data);
-    } else {
-      Swal.fire({
-        title: "Ocurrio un error",
-        text: `no se puede mostrar las canchas en este momento`,
-        icon: "success",
-      });
-    }
-  };
-
+const AdmReservasClientes = () => {
   return (
     <section className="animate-fadeIn space-y-6">
       {/* Header de la sección */}
       <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 bg-slate-400/40 p-6 rounded-2xl border border-slate-300">
         <div>
           <h1 className="text-2xl font-bold text-white">
-            Administración Reservas
+            Administración Reservas 
           </h1>
           <p className="text-zinc-500 text-sm">By RollingClub</p>
         </div>
         {/* <a className="bg-green-500 hover:bg-green-600 text-white px-3 py-2.5 rounded-xl font-bold transition-all shadow-lg shadow-blue-900/20 active:scale-95 gap-2 text-center">
           <i className="bi bi-plus-lg"></i>
-          Alta Reserva
+          Alta Producto
         </a> */}
       </div>
 
@@ -66,25 +37,7 @@ const AdmReservas = () => {
             </tr>
           </thead>
           <tbody className="divide-y divide-slate-400">
-            {reservas.length > 0 ? (
-              reservas.map((Reserva, indice) => (
-                <ItemTablaReserva
-                  key={Reserva._id}
-                  reserva={Reserva}
-                  fila={indice + 1}
-                  setReservas={setReservas}
-                />
-              ))
-            ) : (
-              <tr>
-                <td
-                  colSpan={4}
-                  className="px-6 py-12 text-center text-zinc-500 italic"
-                >
-                  No hay canchas registradas para administrar.
-                </td>
-              </tr>
-            )}
+            <ItemTabla />
           </tbody>
         </table>
       </div>
