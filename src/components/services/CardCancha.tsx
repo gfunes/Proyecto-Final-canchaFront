59fd84abaddeface413446fbfb464e21b1a3d208
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-09-16 17:35:45 -0300

    se cambia nombre del boton de ver cancha a ver Turnos disponibles

--- src/components/services/CardCancha.tsx
+++ src/components/services/CardCancha.tsx
@@ -41,7 +41,7 @@ const CardCancha = ({ cancha, onVerTurnos }: CardCanchaProps) => {
             onClick={() => onVerTurnos(cancha)}
             className="bg-slate-900 hover:bg-green-600 text-white font-bold py-2.5 px-3 rounded-xl shadow-md transition-colors duration-300 transform active:scale-95 uppercase text-[10px] tracking-wider text-center max-w-32.5 cursor-pointer"
           >
-            Ver canchas disponibles
+            Ver Turnos disponibles
           </button>
 
         </div>
